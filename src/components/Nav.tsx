df4941a61e6877169f92b32650b8d2e74ee983b0
lw <1109160884@qq.com>
2022-06-19 11:08:43 +0800

    解决了SVG的问题

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,38 +1,56 @@
-import styled from 'styled-components';
-import {Link} from 'react-router-dom';
-import React from 'react';
-import x from "Icons/Tag.svg"
-
-console.log(x);
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import React from "react";
+// 预防 TreeShaking
+require("Icons/money.svg");
+require("Icons/tag.svg");
+require("Icons/charts.svg");
 const NavWrapper = styled.nav`
   line-height: 24px;
-  box-shadow: 0 0 3px rgba(0,0,0,0.25);
+  box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
   > ul {
     display: flex;
 
     > li {
       width: 33.33%;
-      padding: 16px;
+      padding: 4px 0;
       text-align: center;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      .icon {
+        width: 24px;
+        height: 24px;
+      }
     }
   }
 `;
 
-const Nav = ()=>{
-    return(
-        <NavWrapper>
-            <ul>
-                <li>
-                    <Link to="/tags">标签</Link>
-                </li>
-                <li>
-                    <Link to="/money">记账</Link>
-                </li>
-                <li>
-                    <Link to="/Statistics">统计</Link>
-                </li>
-            </ul>
-        </NavWrapper>
-    )
-}
-export default Nav
+const Nav = () => {
+  return (
+    <NavWrapper>
+      <ul>
+        <li>
+          <svg className="icon">
+            <use xlinkHref="#tag" />
+          </svg>
+          <Link to="/tags">标签</Link>
+        </li>
+        <li>
+          <svg className="icon">
+            <use xlinkHref="#money" />
+          </svg>
+          <Link to="/money">记账</Link>
+        </li>
+        <li>
+          <svg className="icon">
+            <use xlinkHref="#charts" />
+          </svg>
+          <Link to="/Statistics">统计</Link>
+        </li>
+      </ul>
+    </NavWrapper>
+  );
+};
+export default Nav;
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App';
 import "index.scss"
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-    <App />
-);
 
+ReactDOM.render(<App />,document.getElementById("root"))
