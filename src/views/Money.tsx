670ecc4dee3797df8e1d73ded4e6b6ed9498abb8
lw <1109160884@qq.com>
2022-06-23 11:06:44 +0800

    完成标签选择和新增功能

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -1,63 +1,20 @@
 import Layout from "../components/Layout";
 import React from "react";
 import styled from "styled-components";
-const TagsSection = styled.section`
-  background-color: #ffffff;
-  padding: 12px 16px;
-  > ol {
-    margin: 0 -12px;
-    > li {
-      background-color: #d9d9d9;
-      border-radius: 18px;
-      display: inline-block;
-      padding: 3px 18px;
-      font-size: 14px;
-      margin: 8px 12px;
-    }
-  }
-  > button {
-    background-color: none;
-    border: none;
-    padding: 2px 4px;
-    border-bottom: 1px solid #333;
-    color: #666;
-    background-color: #ffffff;
-  }
-`;
+import TagsSection from "./Money/TagsSection";
+import NotesSection from "./Money/NotesSection";
+import CategorySection from "./Money/CategorySection";
+import NumberPadSection from "./Money/NumberPadSection";
 
-const NotesSection = styled.section`
-  background-color: #f5f5f5;
-  padding: 0 16px;
-  font-size: 14px;
-  > label {
-    display: flex;
-    align-items: center;
-    > span {
-      margin-right: 16px;
-      white-space: nowrap;
-    }
-    > input {
-      display: block;
-      width: 100%;
-      height:72px;
-      border: none;
-      background: none;
-    }
-  }
-`;
-const CategorySection = styled.section``;
-const NumberPadSection = styled.section``;
-function Tags() {
+
+const MyLayout = styled(Layout)`
+  display: flex;
+  flex-direction: column;
+`
+function Money() {
   return (
-    <Layout>
+    <MyLayout>
       <TagsSection>
-        <ol>
-          <li>衣</li>
-          <li>食</li>
-          <li>住</li>
-          <li>行</li>
-        </ol>
-        <button>添加标签</button>
       </TagsSection>
       <NotesSection>
         <label>
@@ -67,29 +24,31 @@ function Tags() {
       </NotesSection>
       <CategorySection>
         <ul>
-          <li>支出</li>
+          <li className="selected">支出</li>
           <li>收入</li>
         </ul>
       </CategorySection>
       <NumberPadSection>
-        <div>100</div>
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-        <button>删除</button>
-        <button>4</button>
-        <button>5</button>
-        <button>6</button>
-        <button>清空</button>
-        <button>7</button>
-        <button>8</button>
-        <button>9</button>
-        <button>OK</button>
-        <button>0</button>
-        <button>.</button>
+        <div className="output">100</div>
+        <div className="pad clearfix">
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>删除</button>
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+          <button>清空</button>
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+          <button className="ok">OK</button>
+          <button className="zero">0</button>
+          <button className="dot">.</button>
+        </div>
       </NumberPadSection>
-    </Layout>
+    </MyLayout>
   );
 }
 
-export default Tags;
+export default Money;
---
+++ src/views/Money/TagsSection.tsx
@@ -0,0 +1,75 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+
+const TagsSection: React.FC = (props) => {
+  const [tags, setTags] = useState<string[]>(["衣", "食", "住", "行"]);
+  const [selectedTags, setSelectedTags] = useState<string[]>([]);
+  const tagOnclick = () => {
+    const tagName = window.prompt("请输入标签名");
+    if (tagName !== null) {
+      setTags([...tags, tagName]);
+    }
+  };
+  const onToggleTags = (tag: string) => {
+    const index = selectedTags.indexOf(tag);
+    console.log(index);
+    if (index >= 0) {
+      // 如果tag已经是选中的，就赋值一个含有其他所有没被选中的新对象
+      setSelectedTags(selectedTags.filter((t) => t !== tag));
+    } else {
+      setSelectedTags([...selectedTags, tag]);
+      console.log(selectedTags);
+    }
+  };
+  const getClass = (tag:string)=>selectedTags.indexOf(tag) >= 0 ? "selected" : ""
+  // 注意需要执行的回调函数不能写立即执行的方法，用箭头函数包裹
+  return (
+    <Wrapper>
+      <ol>
+        {tags.map((tag) => (
+          <li
+            key={tag}
+            onClick={() => onToggleTags(tag)}
+            className={getClass(tag)}
+          >
+            {tag}
+          </li>
+        ))}
+      </ol>
+      <button onClick={tagOnclick}>添加标签</button>
+    </Wrapper>
+  );
+}
+const Wrapper = styled.section`
+  background-color: #ffffff;
+  padding: 12px 16px;
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: flex-start;
+  > ol {
+    margin: 0 -12px;
+    > li {
+      background-color: #d9d9d9;
+      border-radius: 18px;
+      display: inline-block;
+      padding: 3px 18px;
+      font-size: 14px;
+      margin: 8px 12px;
+      &.selected {
+        background: #f60;
+      }
+    }
+  }
+  > button {
+    background-color: none;
+    border: none;
+    padding: 2px 4px;
+    border-bottom: 1px solid #333;
+    color: #666;
+    background-color: inherit;
+  }
+`;
+
+export default TagsSection
